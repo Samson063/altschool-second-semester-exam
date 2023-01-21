09f031b9d39e0a144dafe4bb454f18f9c78f778d
Samson063 <furthermaths078@gamil.com>
2023-01-21 23:27:28 +0100

    feat: Added Create New Repository button

--- src/App.js
+++ src/App.js
@@ -1,67 +1,94 @@
-import './App.css';
-import profileImage from './assets/github_img.png';
-import React, { useState } from 'react';
-import RepositoryCard from './component/RepositoryCard';
-import { useEffect } from 'react';
-import axios from "axios"
+import './App.css'
+import profileImage from './assets/github_img.png'
+import React, { useState } from 'react'
+import RepositoryCard from './component/RepositoryCard'
+import { useEffect } from 'react'
+import axios from 'axios'
+import { FaBook } from 'react-icons/fa'
 
 function App() {
+  const [repositories, setRepositories] = useState([])
+  const [isLoading, setIsLoading] = useState(false)
+  const githubURL = 'https://api.github.com/users/samson063/repos'
 
-  const [repositories, setRepositories] = useState([]);
-  const [isLoading, setIsLoading]=useState(false)
-  const githubURL = "https://api.github.com/users/samson063/repos";
-  
-
-  useEffect(()=>{
+  useEffect(() => {
     setIsLoading(true)
-    axios.get(githubURL).then(res=>{
-      setIsLoading(false)
-      setRepositories(res.data)
-    }).catch(err=>{
-      console.log(err.message)
-    })
+    axios
+      .get(githubURL)
+      .then((res) => {
+        setIsLoading(false)
+        setRepositories(res.data)
+      })
+      .catch((err) => {
+        console.log(err.message)
+      })
   }, [])
 
   return (
     <>
-{isLoading && (<p className='loading'>Loading...</p>)}
-{!isLoading && ( <><header className="App-header">
-        <div className='profile'>
-          <img src={profileImage} alt='profile' id='profile_img' />
-          <div className='name'>
-            <h3>Samson063</h3>
-            <button>Edit profile</button>
+      {isLoading && <p className='loading'>Loading...</p>}
+      {!isLoading && (
+        <>
+          <header className='App-header'>
+            <div className='profile'>
+              <img src={profileImage} alt='profile' id='profile_img' />
+              <div className='name'>
+                <h3>Samson063</h3>
+                <button>Edit profile</button>
+              </div>
+            </div>
+            <ul className='navbar-menu'>
+              <li className='navbar-items'>
+                <a href='index.html' className='navbar-links'>
+                  Home
+                </a>
+              </li>
+              <li className='navbar-items'>
+                <a href='Tech.html' className='navbar-links'>
+                  Tech
+                </a>
+              </li>
+              <li className='navbar-items'>
+                <a href='#' className='navbar-links'>
+                  Product
+                </a>
+              </li>
+              <li className='navbar-items'>
+                <a href='#' className='navbar-links'>
+                  Sign Up{' '}
+                </a>
+              </li>
+              <button className='book-botton'>
+                <i className='fabook'>
+                  <FaBook />
+                </i>
+                New
+              </button>
+            </ul>
+          </header>
+          <div className='repositories'>
+            <p>Popular repositories</p>
+            <a href='#' className='navbar-links'>
+              Customise your Pin
+            </a>
           </div>
-        </div>
-        <ul className="navbar-menu">
-          <li className="navbar-items">
-            <a href="index.html" className="navbar-links">Home</a>
-          </li>
-          <li className="navbar-items">
-            <a href="Tech.html" className="navbar-links">Tech</a>
-          </li>
-          <li className="navbar-items">
-            <a href="#" className="navbar-links">Product</a>
-          </li>
-          <li className="navbar-items">
-            <a href="#" className="navbar-links">Sign Up </a>
-          </li>
-        </ul>
-      </header><div className='repositories'>
-          <p>Popular repositories</p>
-          <a href='#' className="navbar-links">Customise your Pin</a>
-        </div><div className='repo-box'>
-         {repositories.map((repo)=>{
-        
-        return  <RepositoryCard name={repo.full_name} visibility={repo.private ? "Private": "Public"} language={repo.language} url={repo.html_url} star_count={repo.stargazers_count} />
-        
-        })}
-
-
-        </div></>) }
-     
+          <div className='repo-box'>
+            {repositories.map((repo) => {
+              return (
+                <RepositoryCard
+                  name={repo.full_name}
+                  visibility={repo.private ? 'Private' : 'Public'}
+                  language={repo.language}
+                  url={repo.html_url}
+                  star_count={repo.stargazers_count}
+                />
+              )
+            })}
+          </div>
+        </>
+      )}
     </>
-  );
+  )
 }
 
-export default App;
+export default App
